92f8a36cff342ad701e48c844b4dbe32edc04994
Witarsana <made.witarsana@gmail.com>
2021-02-12 16:43:34 +0800

    modify templateUrl

--- src/app-ajs/phone-list/phone-list.component.ts
+++ src/app-ajs/phone-list/phone-list.component.ts
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
 // Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
-  });
+angular.module("phoneList").component("phoneList", {
+  templateUrl: "./phone-list.template.html",
+  controller: [
+    "Phone",
+    function PhoneListController(Phone) {
+      this.phones = Phone.query();
+      this.orderProp = "age";
+    },
+  ],
+});
