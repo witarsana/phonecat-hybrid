5604f4acc0b649313a6894415d8c885165486f54
Witarsana <made.witarsana@gmail.com>
2021-02-12 16:40:33 +0800

    import app old library and module inside main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+
+// Import these globally to bring in their @types
+import "angular";
+import "angular-animate";
+import "angular-resource";
+import "angular-route";
+
+// And import our AngularJS module
+import "./app-ajs";
 
 if (environment.production) {
   enableProdMode();
